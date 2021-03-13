d446a92d2649c8da3bfbe9102da79abe5e85957a
BrendowLincoln <linconlbrendow@gmail.com>
2021-03-13 01:33:05 -0300

    Adicionando código demosntrativo para entender como funciona o InheritedWidget

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/app/providers/counter_provider.dart';
 import 'package:shop/app/utils/app_routes.dart';
 import 'package:shop/app/views/product_detail_screen.dart';
 import 'package:shop/app/views/product_overview_screen.dart';
@@ -6,18 +7,20 @@ import 'package:shop/app/views/product_overview_screen.dart';
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Tech Store - Seus gadgets preferidos estão aqui!',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Color.fromRGBO(219, 126, 86, 1),
-        fontFamily: 'Lato',
+    return CounterProvider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Tech Store - Seus gadgets preferidos estão aqui!',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Color.fromRGBO(219, 126, 86, 1),
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL : (context) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        AppRoutes.PRODUCT_DETAIL : (context) => ProductDetailScreen(),
-      },
     );
   }
 }
--- lib/app/views/product_detail_screen.dart
+++ lib/app/views/product_detail_screen.dart
@@ -1,17 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:shop/app/models/product.dart';
+import 'package:shop/app/providers/counter_provider.dart';
 
-class ProductDetailScreen extends StatelessWidget {
+class ProductDetailScreen extends StatefulWidget {
+  @override
+  _ProductDetailScreenState createState() => _ProductDetailScreenState();
+}
 
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
   @override
   Widget build(BuildContext context) {
-
-    final Product product = ModalRoute.of(context)
-        .settings
-        .arguments as Product;
+    final Product product =
+        ModalRoute.of(context).settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
+      ),
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(CounterProvider.of(context).state.value.toString()),
+            SizedBox(height: 20,),
+            ElevatedButton(
+              child: Text("+"),
+              onPressed: () {
+                setState(() {
+                  CounterProvider.of(context).state.increment();
+                  print(CounterProvider.of(context).state.value);
+                });
+              },
+            ),
+            SizedBox(height: 20,),
+            ElevatedButton(
+              child: Text("-"),
+              onPressed: () {
+                setState(() {
+                  if(CounterProvider.of(context).state.value > 0)
+                  CounterProvider.of(context).state.decremente();
+                  print(CounterProvider.of(context).state.value);
+                });
+              },
+            )
+          ],
+        ),
       ),
     );
   }
