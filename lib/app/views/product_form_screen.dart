4668df7d2240e6b2d6ef7ed0a3ab5b5991a51f9c
BrendowLincoln <linconlbrendow@gmail.com>
2021-03-20 19:50:07 -0300

    Fazendo a imagem aparecer a partir na url digitada no campo

--- lib/app/views/product_form_screen.dart
+++ lib/app/views/product_form_screen.dart
@@ -9,12 +9,27 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
 
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocusNode.addListener(_updateImage);
+
+  }
+
+  void _updateImage() {
+    setState(() {});
+  }
 
   @override
   void dispose() {
     super.dispose();
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlFocusNode.removeListener(_updateImage);
+    _imageUrlFocusNode.dispose();
   }
 
 
@@ -57,6 +72,42 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                 ),
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _imageUrlController,
+                      focusNode: _imageUrlFocusNode,
+                      decoration: InputDecoration(
+                        labelText: 'URL da Imagem',
+                      ),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    margin: EdgeInsets.only(top: 8, left: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: _imageUrlController.text.isEmpty
+                    ? Text('Informe a URL')
+                    : FittedBox(
+                      child: Image.network(
+                        _imageUrlController.text,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )
+                ],
               )
 
             ],
